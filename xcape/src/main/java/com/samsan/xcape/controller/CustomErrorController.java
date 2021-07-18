983d9526b2a0a99b5e65c985e4e6ba095b9159f5
Ko juyeong <kapella000@gmail.com>
2021-07-18 17:22:50 +0900

    구글 로그인

--- xcape/src/main/java/com/samsan/xcape/controller/CustomErrorController.java
+++ xcape/src/main/java/com/samsan/xcape/controller/CustomErrorController.java
@@ -1,2 +1,14 @@
-package com.samsan.xcape.controller;public class ErrorController {
+package com.samsan.xcape.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String index() {
+        return "index.html";
+    }
 }
--- xcape/src/main/java/com/samsan/xcape/controller/GoogleLoginController.java
+++ xcape/src/main/java/com/samsan/xcape/controller/GoogleLoginController.java
@@ -17,28 +17,31 @@ import java.security.GeneralSecurityException;
 
 @Controller
 @Log4j2
-public class GoogleLoginCotroller {
+public class GoogleLoginController {
 
     private UserService userService;
 
-    public GoogleLoginCotroller(UserService userService) {
+    public GoogleLoginController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/googleLogin")
-    @ResponseBody
-    public String googleLogin(String idtoken, HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, IOException {
+//    @ResponseBody
+    public void googleLogin(String idtoken, HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, IOException {
         String result = userService.googleLogin(idtoken, request, response);
         response.setHeader("Location", "http://localhost:8080/main");
         response.setStatus(302);
-//        HttpHeaders header = new HttpHeaders();
-//        header.add("Location", "/main");
-//        header
-        return result;
+//        return "/main";
+//        return result;
     }
 
     @GetMapping("/logout")
     public void logout(HttpSession session){
         session.invalidate();
     }
+
+//    @GetMapping("/main")
+//    public void main(){
+//        log.info("main");
+//    }
 }
